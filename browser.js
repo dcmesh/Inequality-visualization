15f5329bd75878b8358ccd2c0a54dd159dfdcf05
Mamadi Jallow <mxj316@case.edu>
2019-02-17 02:59:27 -0500

    Browser BS

--- browser.js
+++ browser.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 
 // The total number of tweets from Columbia
-const query = "select year_ from full_data where year_ > '1960';"
 // try changing airtime to arrdelay in the query
-const query2 =
-  "select country_code_2 from full_data where country_code_2 = 'AD';"
-const defaultQueryOptions = {}
-const connector = new window.MapdCon()
+const query =
+  "select state, year_,avg_fiscal_income,percentile_group from state_ineq";
 
-connector
+const defaultQueryOptions = {}
+//const connector = new window.MapdCon()
+
+/*connector
   .protocol("https")
   .host("use2-api.omnisci.cloud")
   .port("443")
@@ -135,5 +135,79 @@ function createRowChart(data) {
     })
     .attr("height", function(d) {
       return height - y(d.val)
-    })
-}
+    })*/
+//margin of the svg
+
+
+
+
+//get data
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// parse the date / time
+var parseTime = d3.timeParse("%d-%b-%y");
+
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// define the 1st line
+var valueline = d3.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.close); });
+
+// define the 2nd line
+var valueline2 = d3.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.open); });
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+d3.csv("ALL US STATES inequality data.csv", function(error, data) {
+  if (error) throw error;})
+
+  // format the data
+  data.forEach(function(d) {
+      d.date = parseTime(d.date);
+      d.close = +d.close;
+      d.open = +d.open;
+  })
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) {
+	  return Math.max(d.close, d.open); })]);
+
+  // Add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", valueline);
+
+  // Add the valueline2 path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .style("stroke", "red")
+      .attr("d", valueline2);
+
+  // Add the X Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // Add the Y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
